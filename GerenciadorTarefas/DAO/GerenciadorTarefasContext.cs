252e38dc6b34afa517f4869e06da7cec7c15f03d
unknown <andersongabriel96.ag@gmail.com>
2020-01-01 17:33:24 -0300

    Adicionado conexão com mysql

---
+++ GerenciadorTarefas/DAO/GerenciadorTarefasContext.cs
@@ -0,0 +1,14 @@
+﻿using GerenciadorTarefas.Models;
+using MySql.Data.Entity;
+using System.Data.Entity;
+
+namespace GerenciadorTarefas.DAO {
+
+    [DbConfigurationType(typeof(MySqlEFConfiguration))]
+    public class GerenciadorTarefasContext : DbContext {
+
+        public GerenciadorTarefasContext() : base("MySqlPath") { }
+
+        public DbSet<Administrador> Administrador { get; set; }
+    }
+}
